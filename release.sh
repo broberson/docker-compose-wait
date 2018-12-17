c77137d358a5db4d2669be9b83c516d678c7dc2d
Brandon Roberson <broberson@gmail.com>
2018-12-17 09:14:01 +0000

    added armv7-unknown-linux-gnueabihf target for raspberry pi build

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 cargo test
 
 cargo build --release --target=x86_64-unknown-linux-musl
+cargo build --release --target=armv7-unknown-linux-gnueabihf
 
-strip ./target/x86_64-unknown-linux-musl/release/wait
+x86_64-linux-gnu-strip ./target/x86_64-unknown-linux-musl/release/wait
+arm-linux-gnueabihf-strip ./target/armv7-unknown-linux-gnueabihf/release/wait
